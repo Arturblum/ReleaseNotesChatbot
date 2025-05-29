2bfb4317db95105b069c430afb663f2de5981862
Arturblum <93647826+Arturblum@users.noreply.github.com>
2025-05-29 22:13:31 +0300

    Add initial project structure and Git integration with configuration files

---
+++ ReleaseNotesChatbot/GitPlugin.cs
@@ -0,0 +1,73 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using LibGit2Sharp;
+
+namespace ReleaseNotesChatbot;
+
+public class GitPlugin
+{
+    private string _repoPath;
+    
+    //kernel funtion to set the repo path
+    [KernelFunction("set_repo_path")]
+    [Description(
+        "Sets the path to the Git repository when the first request to get commits is passed or if the user asks to work with a different repo. if the rpeo isnt set the path will not be changed")]
+    public string SetRepoPath(
+        [Description("The path to the Git repository.")]
+        string repoPath)
+    {
+        try
+        {
+            var repoRoot = Repository.Discover(repoPath);
+            if (repoRoot == null)
+            {
+                Console.WriteLine($"No Git repo found at or above: {repoPath}");
+                return $"No Git repo found at or above: {repoPath}";
+            }
+
+            _repoPath = repoPath;
+            Console.WriteLine($"Repository path set to: {_repoPath}");
+            return $"Repository path set to: {_repoPath}";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+
+    [KernelFunction("get_latest_commits")]
+    [Description("Gets a list of the latests commits from a Git repository.")]
+    public List<string> GetLatestCommits(
+        // [Description("The path to the Git repository.")]
+        // string repoPath,
+        [Description("The number of latest commits to retrieve.")]
+        int count)
+    {
+        var latestCommits = new List<Commit>();
+        var stringifiedCommits = new List<string>();
+
+        try
+        {
+            var repoRoot = Repository.Discover(_repoPath);
+            if (repoRoot == null)
+            {
+                Console.WriteLine($"No Git repo found at or above: {_repoPath}");
+                return stringifiedCommits;
+            }
+
+            using var repo = new Repository(repoRoot);
+            foreach (var commit in repo.Commits.Take(count))
+            {
+                stringifiedCommits.Add(commit.Message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading commits: {ex.Message}");
+        }
+
+        return stringifiedCommits;
+    }
+}
